f87bda37d33b377294fc3eeeb5f9d71a007be122
Ishan Patel <i@patelishan.com>
2018-04-21 18:11:35 -0400

    Profile Edit Page

--- src/main/java/com/musixplayer/controller/ProfileController.java
+++ src/main/java/com/musixplayer/controller/ProfileController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.Principal;
@@ -74,6 +75,43 @@ public class ProfileController {
         }
 
         modelAndView.setViewName("redirect: /");
+        return modelAndView;
+    }
+
+    @PostMapping("/{username}/edit")
+    public ModelAndView editProfileDetails(ModelAndView modelAndView, RedirectAttributes redir, @PathVariable("username") String username, @RequestParam Map requestParams, Principal principal) {
+        String requestURI = "/profile/" + username + "/edit";
+
+        String email = (String) requestParams.get("email");
+        String firstName = (String) requestParams.get("firstname");
+        String lastName = (String) requestParams.get("lastname");
+
+        Person loggedinPerson = personService.findByUsername(principal.getName()).orElse(null);
+        Person toEditPerson = personService.findByUsername(username).orElse(null);
+
+        if(loggedinPerson.getUsername().equals(toEditPerson.getUsername()) || loggedinPerson.getRole().equals("EDITOR") || loggedinPerson.getRole().equals("ADMIN")){
+            if(email!=null){
+                toEditPerson.setEmail(email);
+                String emailHash;
+                try {
+                    MessageDigest m=MessageDigest.getInstance("MD5");
+                    m.update(email.getBytes(),0,email.length());
+                    emailHash = new BigInteger(1,m.digest()).toString(16);
+                } catch (NoSuchAlgorithmException e) {
+                    emailHash="";
+                }
+                toEditPerson.setEmailHash(emailHash);
+            }
+            if(firstName!=null){
+                toEditPerson.setFirstName(firstName);
+            }
+            if(lastName!=null){
+                toEditPerson.setLastName(lastName);
+            }
+            personService.create(toEditPerson);
+        }
+
+        modelAndView.setViewName("redirect:" + requestURI);
         return modelAndView;
     }
 
